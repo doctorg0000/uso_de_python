3a645d8a3b2bc508380628ad2e90bef552305a5e
doctorg0000 <zulcob12@gmail.com>
2025-03-20 19:59:51 -0500

    Cambios_20/03/2025

--- Tipo_de_operadores.py
+++ Tipo_de_operadores.py
@@ -4,6 +4,71 @@ operadores aritmeticos
 
 '''
 
-#operador suma
+# operador suma
 
-print(2+6)
+# para realiza comentarios se utilizan los operadores de comillas triples (' y # )
+
+
+# print(2+6) 
+# print(2*8)
+
+# #guardar variable colocando nombre y su valor antes de la operacion
+
+# x=4
+# y=4
+# print(x*y)
+
+#operador division
+
+# z=12/4
+# print(z)
+# print(type(z))#el resultado de la divison siempre es flotante
+
+# print(10/3)
+# print(10//3)
+# print(14.5//3)
+
+#la division siempre devuelve el entero mas proximo
+
+# print(20/3)
+# print(20%3)
+
+#el "%" se utiliza como modulo que es eñ residuo en una operacion 
+
+#
+# print(8//-3)
+# print (8/-3)
+
+#para expresar potencia es utilizando dos veces **
+
+# print(5**2)
+
+'''
+Este tipo de operadores los usamos cuando deseamos comparar expresiones o
+variables. Python evalúa si se cumple la comparación y nos devuelve (retorna) un
+valor True o False
+
+'''
+
+# print(8==8)
+
+# print(3==3.01)
+
+# # print(3!=1)
+# saldo=100
+# saldo+=9
+
+# print(saldo)
+# x=7
+# y=6
+# print(x==6 and y==6)
+# print(x==6 or y==6)
+
+usuario_logueado=True
+click_logout=True
+print(not usuario_logueado)
+print(usuario_logueado)
+
+
+
+
